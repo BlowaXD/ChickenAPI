c188704850790e363609a4defa81417beab31866
Blowa <blowaxd693@gmail.com>
2018-05-09 20:16:38 +0200

    add mapdto

---
+++ src/ChickenAPI/Dtos/MapDto.cs
@@ -0,0 +1,14 @@
+﻿namespace ChickenAPI.Dtos
+{
+    public class MapDto
+    {
+        public short Id { get; set; }
+        public string Name { get; set; }
+        public bool AllowShop { get; set; }
+        public bool AllowPvp { get; set; }
+        public int Music { get; set; }
+        public short Height { get; set; }
+        public short Width { get; set; }
+        public byte[] Grid { get; set; }
+    }
+}
