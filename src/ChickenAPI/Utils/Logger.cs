ce12051bd59b46f6c969e80a08cb0a9a70305f2b
Syl Eze <syleze.contact@gmail.com>
2018-05-17 12:05:40 +0200

    basic logger: 	- add generic method to get a Logger object from class 	- add Trace, Debug, Info, Error, Fatal methods

--- src/ChickenAPI/Utils/Logger.cs
+++ src/ChickenAPI/Utils/Logger.cs
@@ -1,9 +1,51 @@
-﻿using log4net;
+﻿using System;
+using NLog;
 
 namespace ChickenAPI.Utils
 {
-    public static class Logger
+    public class Logger
     {
-        public static ILog Log { get; set; }
+        private ILogger _log { get; set; }
+
+        private Logger(Type type)
+        {
+            _log = LogManager.GetLogger(type.ToString());
+        }
+
+        public static Logger GetLogger<TClass>() 
+            where TClass : class
+        {
+            return new Logger(typeof(TClass));
+        }
+
+        public void Trace(string msg)
+        {
+            _log?.Trace(msg);
+        }
+
+        public void Debug(string msg)
+        {
+            _log?.Debug(msg);
+        }
+
+        public void Info(string msg)
+        {
+            _log?.Info(msg);
+        }
+
+        public void Warn(string msg)
+        {
+            _log?.Warn(msg);
+        }
+
+        public void Error(string msg, Exception ex)
+        {
+            _log?.Error(ex, msg);
+        }
+
+        public void Fatal(string msg, Exception ex)
+        {
+            _log?.Fatal(ex, msg);
+        }
     }
 }
