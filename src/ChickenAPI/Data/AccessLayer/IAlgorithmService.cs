b358722c413655000de736ff27d3c953315b58d1
Kraken <kraken.kk1@gmail.com>
2018-05-23 15:19:04 +0200

    add IAlgorithmService

---
+++ src/ChickenAPI/Data/AccessLayer/IAlgorithmService.cs
@@ -0,0 +1,136 @@
+﻿using ChickenAPI.Enums.Game.Character;
+using ChickenAPI.Enums.Game.Items;
+
+namespace ChickenAPI.Data.AccessLayer
+{
+    /// <summary>
+    /// IAlgorithmService should provide the methods to recover some algorithm based datas of the game such as Xp, JobXp...
+    /// </summary>
+    public interface IAlgorithmService
+    {
+        /// <summary>
+        /// This method will search through algorithm service and return the LevelXp stat based on <see cref="CharacterClassType"/> and level
+        /// /!\ Should return the highest value under level if level is out of range
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        int GetLevelXp(CharacterClassType type, int level);
+
+        /// <summary>
+        /// This method will search through algorithm service and return the JobLevelXp stat based on <see cref="CharacterClassType"/> and level
+        /// /!\ Should return the highest value under level if level is out of range
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        int GetJobLevelXp(CharacterClassType type, int level);
+
+        /// <summary>
+        /// This method will search through algorithm service and return the HeroLevelXp stat based on <see cref="CharacterClassType"/> and level
+        /// /!\ Should return the highest value under level if level is out of range
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        int GetHeroLevelXp(CharacterClassType type, int level);
+
+        /// <summary>
+        /// This method will search through algorithm service and return the SpLevelXp stat based on <see cref="SpType"/> and level
+        /// /!\ Should return the highest value under level if level is out of range
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        int GetSpLevelXp(SpType type, int level);
+
+        /// <summary>
+        /// This method will search through algorithm service and return the Fairy Level Xp stat based on the fairy's level
+        /// /!\ Should return the highest value under level if level is out of range
+        /// </summary>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        int GetFairyLevelXp(int level);
+
+        /// <summary>
+        /// This method will search through algorithm service and return the Fairy Level Xp stat based on the fairy's level
+        /// /!\ Should return the highest value under level if level is out of range
+        /// </summary>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        int GetFamilyLevelXp(int level);
+
+        /// <summary>
+        /// This method will search through algorithm service and return the Speed stat based on <see cref="CharacterClassType"/> and level
+        /// /!\ Should return the highest value under level if level is out of range
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        int GetSpeed(CharacterClassType type, int level);
+
+        /// <summary>
+        /// This method will search through algorithm service and return the Close Defence stat based on <see cref="CharacterClassType"/> and level
+        /// /!\ Should return the highest value under level if level is out of range
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        int GetDefenceClose(CharacterClassType type, int level);
+
+        /// <summary>
+        /// This method will search through algorithm service and return the Ranged Defence stat based on <see cref="CharacterClassType"/> and level
+        /// /!\ Should return the highest value under level if level is out of range
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        int GetDefenceRange(CharacterClassType type, int level);
+
+        /// <summary>
+        /// This method will search through algorithm service and return the Magic Defence stat
+        /// /!\ Should return the highest value under level if level is out of range
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        int GetDefenceMagic(CharacterClassType type, int level);
+
+        /// <summary>
+        /// This method will search through algorithm service and return the Dodge close stat based on <see cref="CharacterClassType"/> and level
+        /// /!\ Should return the highest value under level if level is out of range
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        int GetDodgeClose(CharacterClassType type, int level);
+
+        /// <summary>
+        /// This method will search through algorithm service and return the Dodge Ranged stat based on <see cref="CharacterClassType"/> and level
+        /// /!\ Should return the highest value under level if level is out of range
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        int GetDodgeRanged(CharacterClassType type, int level);
+
+        /// <summary>
+        /// This method will search through algorithm service and return the Dodge Magic stat based on <see cref="CharacterClassType"/> and level
+        /// /!\ Should return the highest value under level if level is out of range
+        /// /!\/!\ Even if the base game logic tells magic attack does not miss, you can customise this :)
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        int GetDodgeMagic(CharacterClassType type, int level);
+
+        /// <summary>
+        /// This method will search through algorithm service and return the minimum attack range stat based on <see cref="CharacterClassType"/> and level
+        /// /!\ Should return the highest value under level if level is out of range
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        int GetMinimumAttackRange(CharacterClassType type, int level);
+    }
+}
---
+++ src/ChickenAPI/Enums/Game/Items/SpType.cs
@@ -0,0 +1,33 @@
+﻿namespace ChickenAPI.Enums.Game.Items
+{
+    public enum SpType
+    {
+        Pirat,
+        Pajama,
+        Sp1E,
+        Sp1A,
+        Sp1M,
+        Sp2E,
+        Sp2A,
+        Sp2M,
+        Jajamaru,
+        Sp3E,
+        Sp3A,
+        Sp3M,
+        Sp4E,
+        Sp4A,
+        Sp4M,
+        Sp5E,
+        Sp5A,
+        Sp5M,
+        Sp6E,
+        Sp6A,
+        Sp6M,
+        Sp7E,
+        Sp7A,
+        Sp7M,
+        Sp8E,
+        Sp8A,
+        Sp8M,
+    }
+}
