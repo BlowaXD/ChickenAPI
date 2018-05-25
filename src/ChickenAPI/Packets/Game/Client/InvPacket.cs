a91e033ee370cda756853b8a59f257d48efa8c23
Unknown <godnesscookie@gmail.com>
2018-05-25 20:03:08 +0200

    add InvPacket add MviPacket fix MlobjPacket class name

---
+++ src/ChickenAPI/Packets/Game/Client/InvPacket.cs
@@ -0,0 +1,24 @@
+﻿namespace ChickenAPI.Packets.Game.Client
+{
+    [PacketHeader("inv")]
+    public class InvPacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public short InventoryType { get; set; }
+
+        [PacketIndex(1)]
+        public short InventorySlot { get; set; }
+
+        [PacketIndex(2)]
+        public long ItemVNum { get; set; }
+
+        [PacketIndex(3)]
+        public short Rare { get; set; }
+
+        [PacketIndex(4)]
+        public short Upgrade { get; set; }
+
+        [PacketIndex(5)]
+        public byte Unknown2 { get; set; } // looks like always 0
+    }
+}
---
+++ src/ChickenAPI/Packets/Game/Client/MviPacket.cs
@@ -0,0 +1,18 @@
+﻿namespace ChickenAPI.Packets.Game.Client
+{
+    [PacketHeader("mvi")]
+    public class MviPacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public short InventoryType { get; set; }
+
+        [PacketIndex(1)]
+        public short InventorySlot { get; set; }
+
+        [PacketIndex(2)]
+        public short Amount { get; set; }
+
+        [PacketIndex(3)]
+        public short DestinationSlot { get; set; }
+    }
+}
