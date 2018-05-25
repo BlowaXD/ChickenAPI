53581875d3ef2d9e52113e5d3efaebf22f31dc59
Blowa <blowaxd693@gmail.com>
2018-05-25 16:14:27 +0200

    like a potator but for documentation

---
+++ src/ChickenAPI.PacketGeneratorCLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChickenAPI.PacketGeneratorCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PacketMdGenerator.DoWork(args[0]);
+        }
+    }
+}
