a3d7386dbc26ade4d0ef2b4c7186be9004e0e706
Syl Eze <syleze.contact@gmail.com>
2018-05-27 00:11:06 +0200

    basis for inventory system

---
+++ src/ChickenAPI/Game/Systems/Inventory/InventoryAddItemEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using ChickenAPI.ECS.Systems;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ChickenAPI.Data.TransferObjects;
+
+namespace ChickenAPI.Game.Systems.Inventory
+{
+    public class InventoryAddItemEventArgs : SystemEventArgs
+    {
+        public ItemDto Item { get; set; }
+
+        public short Amount { get; set; }
+    }
+}
