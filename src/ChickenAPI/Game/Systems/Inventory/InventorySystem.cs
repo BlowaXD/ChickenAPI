c69cd309f0e3941f5f2da47b29ac1e86ebc1297a
Syl Eze <syleze.contact@gmail.com>
2018-05-27 00:11:06 +0200

    basis for inventory system

---
+++ src/ChickenAPI/Game/Systems/Inventory/InventorySystem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ChickenAPI.ECS.Entities;
+using ChickenAPI.ECS.Systems;
+using ChickenAPI.Game.Components;
+
+namespace ChickenAPI.Game.Systems.Inventory
+{
+    public class InventorySystem : NotifiableSystemBase
+    {
+        public InventorySystem(IEntityManager em) : base(em)
+        {
+
+        }
+
+        public override void Execute(IEntity entity, SystemEventArgs e)
+        {
+            var inventory = entity.GetComponent<InventoryComponent>();
+
+            switch (e)
+            {
+                case InventoryAddItemEventArgs addItemEventArgs:
+                    AddItem(inventory, addItemEventArgs);
+                    break;
+            }
+        }
+
+        private void AddItem(InventoryComponent inv, InventoryAddItemEventArgs args)
+        {
+
+        }
+    }
+}
