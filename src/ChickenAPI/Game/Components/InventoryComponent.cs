ef20a2f40a46391b170b98ab024690a818bfc0db
Syl Eze <syleze.contact@gmail.com>
2018-05-27 23:08:07 +0200

    separate each type of inventories into different arrays

--- src/ChickenAPI/Game/Components/InventoryComponent.cs
+++ src/ChickenAPI/Game/Components/InventoryComponent.cs
@@ -7,17 +7,31 @@ namespace ChickenAPI.Game.Components
 {
     public class InventoryComponent : IComponent
     {
-        public const byte INVENTORY_SIZE = 196;
-
-        public const short MAX_ITEMS_PER_SLOT = 999;
+        private const byte WEAR_SIZE = 16;
+        private const byte EQUIPMENT_SIZE = 48;
+        private const byte MAIN_SIZE = 48;
+        private const byte ETC_SIZE = 48;
 
         public InventoryComponent(IEntity entity)
         {
-            ItemInstances = new ItemInstanceDto[INVENTORY_SIZE];
+            Wear = new ItemInstanceDto[WEAR_SIZE];
+            Equipment = new ItemInstanceDto[EQUIPMENT_SIZE];
+            Main = new ItemInstanceDto[MAIN_SIZE];
+            Etc = new ItemInstanceDto[ETC_SIZE];
             Entity = entity;
         }
 
-        public ItemInstanceDto[] ItemInstances { get; set; }
+        public ItemInstanceDto[] Equipment { get; set; }
+
+        public ItemInstanceDto[] Wear { get; set; }
+
+        public ItemInstanceDto[] Main { get; set; }
+
+        public ItemInstanceDto[] Etc { get; set; }
+
+        public ItemInstanceDto[] Specialists { get; set; }
+
+        public ItemInstanceDto[] Costumes { get; set; }
 
         public IEntity Entity { get; }
     }
