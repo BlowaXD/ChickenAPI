d9d9b638cbc0170203f2ecfc69c6d044e00dcd9e
Blowa <blowaxd693@gmail.com>
2018-05-27 21:50:46 +0200

    begin MapNpcMonster

---
+++ src/ChickenAPI/Data/TransferObjects/NpcMonsterSkillDto.cs
@@ -0,0 +1,14 @@
+﻿using ChickenAPI.Data.AccessLayer.Repository;
+
+namespace ChickenAPI.Data.TransferObjects
+{
+    public class NpcMonsterSkillDto : IMappedDto
+    {
+        /// <summary>
+        /// Can be considered as the skill vnum
+        /// </summary>
+        public long Id { get; set; }
+        public short Rate { get; set; }
+        public long NpcMonsterId { get; set; }
+    }
+}
