cccc8e2320dd1cda8d7767eecc9826f9b3a92ef0
Syl Eze <syleze.contact@gmail.com>
2018-05-27 23:08:07 +0200

    separate each type of inventories into different arrays

--- src/ChickenAPI/Game/Systems/Inventory/InventorySystem.cs
+++ src/ChickenAPI/Game/Systems/Inventory/InventorySystem.cs
@@ -1,14 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
+using ChickenAPI.Data.TransferObjects;
 using ChickenAPI.ECS.Entities;
 using ChickenAPI.ECS.Systems;
+using ChickenAPI.Enums.Game.Items;
 using ChickenAPI.Game.Components;
 
 namespace ChickenAPI.Game.Systems.Inventory
 {
     public class InventorySystem : NotifiableSystemBase
     {
+        private const short MAX_AMOUNT_PER_SLOT = 999;
+
         public InventorySystem(IEntityManager em) : base(em)
         {
 
@@ -28,7 +31,49 @@ namespace ChickenAPI.Game.Systems.Inventory
 
         private void AddItem(InventoryComponent inv, InventoryAddItemEventArgs args)
         {
+            ItemInstanceDto[] subinv;
 
+            switch (args.Item.Type)
+            {
+                case InventoryType.Wear:
+                    subinv = inv.Wear;
+                    break;
+                case InventoryType.Equipment:
+                    subinv = inv.Equipment;
+                    break;
+                case InventoryType.Main:
+                    subinv = inv.Main;
+                    break;
+                case InventoryType.Etc:
+                    subinv = inv.Etc;
+                    break;
+                default:
+                    return;
+            }
+
+            var slot = GetFirstFreeSlot(subinv);
+
+            if (slot == -1)
+            {
+                //NOT ENOUGH SPACE
+                return;
+            }
+            subinv.Append(new ItemInstanceDto());
+        }
+
+        private int GetFirstFreeSlot(ItemInstanceDto[] subinventory)
+        {
+            for (var i = 0; i < subinventory.Length; i++)
+            {
+                var item = subinventory.First(x => x.Slot == i);
+
+                if (item == null)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
         }
     }
 }
