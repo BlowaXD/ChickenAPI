f0047206d08de1d64e6215805f59a7f1befd3ae5
PonyWarrior <lob.revolu@gmail.com>
2018-05-27 13:12:06 +0200

    Add RestPacket for Client and Server

---
+++ src/ChickenAPI/Packets/Game/Client/RestPacket.cs
@@ -0,0 +1,15 @@
+﻿namespace ChickenAPI.Packets.Game.Client
+{
+    [PacketHeader("rest")]
+    public class RestPacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public byte Unknown1 { get; set; } // always set to 1
+
+        [PacketIndex(1)]
+        public byte Unknown2 { get; set; } // always set to 1
+
+        [PacketIndex(2)]
+        public int PlayerID { get; set; }
+    }
+}
---
+++ src/ChickenAPI/Packets/Game/Server/RestPacket.cs
@@ -0,0 +1,16 @@
+﻿namespace ChickenAPI.Packets.Game.Server
+{
+    [PacketHeader("rest")]
+    public class RestPacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public byte Unknown1 { get; set; } // always set to 1
+
+        [PacketIndex(1)]
+        public int PlayerID { get; set; }
+
+        [PacketIndex(2)]
+        public byte Unknown2 { get; set; } // always set to 0
+    }
+}
+
