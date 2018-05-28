2b9754275f3a457242f7ba53f00dc42b03bd5465
Blowa <blowaxd693@gmail.com>
2018-05-28 14:59:41 +0200

    add NpcEntity & MonsterEntity

---
+++ src/ChickenAPI/Game/Entities/Monster/MonsterEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using ChickenAPI.ECS.Components;
+using ChickenAPI.ECS.Entities;
+using ChickenAPI.Enums.Game.Entity;
+using ChickenAPI.Game.Components;
+
+namespace ChickenAPI.Game.Entities.Monster
+{
+    public class MonsterEntity : EntityBase
+    {
+        public MonsterEntity() : base(EntityType.Monster)
+        {
+            Components = new Dictionary<Type, IComponent>
+            {
+                { typeof(VisibilityComponent), new VisibilityComponent(this) },
+                { typeof(BattleComponent), new BattleComponent(this) },
+                { typeof(MovableComponent), new MovableComponent(this) }
+            };
+        }
+
+        public override void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
