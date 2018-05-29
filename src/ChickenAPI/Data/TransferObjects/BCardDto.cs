ff7736f3f997ead9816afea3b158dbe7be43e397
Blowa <blowaxd693@gmail.com>
2018-05-29 22:31:59 +0200

    fix bcardDto/SkillDto

--- src/ChickenAPI/Data/TransferObjects/BCardDto.cs
+++ src/ChickenAPI/Data/TransferObjects/BCardDto.cs
@@ -5,14 +5,29 @@ namespace ChickenAPI.Data.TransferObjects
     public class BCardDto : IMappedDto
     {
         public long Id { get; set; }
-        public long NpcMonsterId { get; set; }
-        public byte Type { get; set; }
+
         public byte SubType { get; set; }
+
+        public byte Type { get; set; }
+
+        public int FirstData { get; set; }
+
+        public int SecondData { get; set; }
+
+        public int ThirdData { get; set; }
+
+        public long? CardId { get; set; }
+
+        public long? ItemId { get; set; }
+
+        public long? SkillId { get; set; }
+
+        public long? NpcMonsterId { get; set; }
+
+        public byte CastType { get; set; }
+
         public bool IsLevelScaled { get; set; }
+
         public bool IsLevelDivided { get; set; }
-        public short FirstData { get; set; }
-        public short SecondData { get; set; }
-        public short ThirdData { get; set; }
-        public int CastType { get; set; }
     }
 }
