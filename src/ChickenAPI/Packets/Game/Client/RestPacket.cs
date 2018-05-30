7d1a1a103593da122497ccede1bfb466c28c9f85
PonyWarrior <lob.revolu@gmail.com>
2018-05-30 16:52:08 +0200

    Update RestPacket.cs

--- src/ChickenAPI/Packets/Game/Client/RestPacket.cs
+++ src/ChickenAPI/Packets/Game/Client/RestPacket.cs
@@ -4,12 +4,12 @@
     public class RestPacket : PacketBase
     {
         [PacketIndex(0)]
-        public byte Unknown1 { get; set; } // always set to 1
+        public byte VisualType { get; set; } // always set to 1
 
         [PacketIndex(1)]
-        public byte Unknown2 { get; set; } // always set to 1
+        public byte VisualType { get; set; } // always set to 1
 
         [PacketIndex(2)]
-        public int PlayerId { get; set; }
+        public int EntityId { get; set; }
     }
 }
