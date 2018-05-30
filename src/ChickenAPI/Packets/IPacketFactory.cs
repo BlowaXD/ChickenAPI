e592f46154c84bf158eabcd8e9bbdf7519fe2958
Blowa <blowaxd693@gmail.com>
2018-05-30 10:41:30 +0200

    fix packetSerialization invoking reflection (slow)

--- src/ChickenAPI/Packets/IPacketFactory.cs
+++ src/ChickenAPI/Packets/IPacketFactory.cs
@@ -5,6 +5,9 @@ namespace ChickenAPI.Packets
     public interface IPacketFactory
     {
         string Serialize<TPacket>(TPacket packet) where TPacket : IPacket;
+
+        string Serialize(IPacket packet, Type type);
+
         IPacket Deserialize(string packetContent, Type packetType, bool includesKeepAliveIdentity);
     }
 }
