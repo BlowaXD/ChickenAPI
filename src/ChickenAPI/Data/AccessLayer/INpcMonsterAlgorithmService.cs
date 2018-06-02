9c4ef14d5a16ad789323416989fabe725c4f7372
Blowa <blowaxd693@gmail.com>
2018-06-02 12:13:04 +0200

    add INpcMonsterAlgorithmService

---
+++ src/ChickenAPI/Data/AccessLayer/INpcMonsterAlgorithmService.cs
@@ -0,0 +1,17 @@
+﻿using ChickenAPI.Enums.Game.Entity;
+
+namespace ChickenAPI.Data.AccessLayer
+{
+    public interface INpcMonsterAlgorithmService
+    {
+        /// <summary>
+        /// Returns the MaxHp based on basic algorithm + special races stats
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="level"></param>
+        /// <param name="isMonster"></param>
+        /// <returns></returns>
+        int GetHpMax(NpcMonsterRaceType type, byte level, bool isMonster);
+        int GetMpMax(NpcMonsterRaceType type, byte level, bool isMonster);
+    }
+}
