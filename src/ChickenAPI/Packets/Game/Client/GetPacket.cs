3e401edc97ad5d0fc208b82cce21577ed698f41d
Unknown <godnesscookie@gmail.com>
2018-06-07 17:32:20 +0200

    Add b_i/get/put/wear-Packet

---
+++ src/ChickenAPI/Packets/Game/Client/GetPacket.cs
@@ -0,0 +1,18 @@
+﻿namespace ChickenAPI.Packets.Game.Client
+{
+    [PacketHeader("get")]
+    public class GetPacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public byte Unknown { get; set; } // seems to be always 1
+
+        [PacketIndex(1)]
+        public short CharacterID { get; set; }
+
+        [PacketIndex(2)]
+        public long DropID { get; set; }
+
+        [PacketIndex(3)]
+        public byte Unknown2 { get; set; } // seems to be always 0
+    }
+}
