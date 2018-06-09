7e8ad356c96d50eecf32218212a9020f8b1c5809
Blowa <blowaxd693@gmail.com>
2018-06-09 21:18:04 +0200

    add BcardRelationType to differenciate different bcards

--- src/ChickenAPI/Data/TransferObjects/BCard/BCardDto.cs
+++ src/ChickenAPI/Data/TransferObjects/BCard/BCardDto.cs
@@ -1,4 +1,5 @@
 ﻿using ChickenAPI.Data.AccessLayer.Repository;
+using ChickenAPI.Enums.Game.BCard;
 
 namespace ChickenAPI.Data.TransferObjects
 {
@@ -6,28 +7,7 @@ namespace ChickenAPI.Data.TransferObjects
     {
         public long Id { get; set; }
 
-        public byte SubType { get; set; }
-
-        public byte Type { get; set; }
-
-        public int FirstData { get; set; }
-
-        public int SecondData { get; set; }
-
-        public int ThirdData { get; set; }
-
-        public long? CardId { get; set; }
-
-        public long? ItemId { get; set; }
-
-        public long? SkillId { get; set; }
-
-        public long? NpcMonsterId { get; set; }
-
-        public byte CastType { get; set; }
-
-        public bool IsLevelScaled { get; set; }
-
-        public bool IsLevelDivided { get; set; }
+        public BCardRelationType RelationType { get; set; }
+        public long RelationId { get; set; }
     }
 }
