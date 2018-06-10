3cdfd2e622257c66a60d22cd3d08de4a9ce97e40
Blowa <blowaxd693@gmail.com>
2018-06-10 18:13:32 +0200

    update packages & fix characterskillDto

--- src/ChickenAPI/Data/TransferObjects/Character/CharacterSkillDto.cs
+++ src/ChickenAPI/Data/TransferObjects/Character/CharacterSkillDto.cs
@@ -6,5 +6,6 @@ namespace ChickenAPI.Data.TransferObjects.Character
     public class CharacterSkillDto : ISynchronizedDto
     {
         public Guid Id { get; set; }
+        public long SkillId { get; set; }
     }
 }
