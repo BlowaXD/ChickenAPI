0bf19dea3dedb139f4cf0c7e13920c763e669f7e
Kraken <kraken.kk1@gmail.com>
2018-06-10 15:48:35 +0200

    fix BCardDto

--- src/ChickenAPI/Data/TransferObjects/BCard/BCardDto.cs
+++ src/ChickenAPI/Data/TransferObjects/BCard/BCardDto.cs
@@ -7,7 +7,24 @@ namespace ChickenAPI.Data.TransferObjects.BCard
     {
         public long Id { get; set; }
 
+        public byte SubType { get; set; }
+
+        public byte Type { get; set; }
+
+        public int FirstData { get; set; }
+
+        public int SecondData { get; set; }
+
+        public int ThirdData { get; set; }
+
+        public byte CastType { get; set; }
+
+        public bool IsLevelScaled { get; set; }
+
+        public bool IsLevelDivided { get; set; }
+
         public BCardRelationType RelationType { get; set; }
+
         public long RelationId { get; set; }
     }
 }
