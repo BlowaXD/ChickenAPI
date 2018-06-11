ab92881e00a97542a97ad26b5014e4f857f9cd3f
Blowa <blowaxd693@gmail.com>
2018-06-11 16:42:01 +0200

    Correct IBcardService to be sure to get the good Dto

--- src/ChickenAPI/Data/AccessLayer/BCard/IBCardService.cs
+++ src/ChickenAPI/Data/AccessLayer/BCard/IBCardService.cs
@@ -2,11 +2,14 @@
 using System.Threading.Tasks;
 using ChickenAPI.Data.AccessLayer.Repository;
 using ChickenAPI.Data.TransferObjects.BCard;
+using ChickenAPI.Enums.Game.BCard;
 
 namespace ChickenAPI.Data.AccessLayer.BCard
 {
     public interface IBCardService : IMappedRepository<BCardDto>
     {
+        Task<BCardDto> GetByIdAndType(long id, BCardRelationType type);
+
         Task<IEnumerable<BCardDto>> GetBySkillIdAsync(long skillId);
 
         Task<IEnumerable<BCardDto>> GetByMapMonsterIdAsync(long monsterId);
