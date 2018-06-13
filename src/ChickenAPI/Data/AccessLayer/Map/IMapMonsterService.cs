0b6528785bbbe1cb3b09b1a35660de897476e8e1
Blowa <blowaxd693@gmail.com>
2018-06-13 21:58:58 +0200

    add sync GetByMapId on MapObjects Services

--- src/ChickenAPI/Data/AccessLayer/Map/IMapMonsterService.cs
+++ src/ChickenAPI/Data/AccessLayer/Map/IMapMonsterService.cs
@@ -3,10 +3,11 @@ using System.Threading.Tasks;
 using ChickenAPI.Data.AccessLayer.Repository;
 using ChickenAPI.Data.TransferObjects.Map;
 
-namespace ChickenAPI.Data.AccessLayer.NpcMonster
+namespace ChickenAPI.Data.AccessLayer.Map
 {
     public interface IMapMonsterService : IMappedRepository<MapMonsterDto>
     {
+        IEnumerable<MapMonsterDto> GetByMapId(long mapId);
         Task<IEnumerable<MapMonsterDto>> GetByMapIdAsync(long mapId);
     }
 }
